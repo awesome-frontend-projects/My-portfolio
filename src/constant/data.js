63efa86cc296776dd2d0034ffcd6ecc9eb85b1de
Medhanye Tsegay <166837590+awesome-frontend-projects@users.noreply.github.com>
2025-11-28 00:14:40 -0800

    Update image path for Financial Dashboard project

--- src/constant/data.js
+++ src/constant/data.js
@@ -188,7 +188,7 @@ export const timelineItems = [
 export const projectsItems = [
   {
     id: 1,
-    img: "/images/portfolio-.png",
+    img: "/images/portfolio-12.png",
     title: "Financial Dashboard",
     text: "A fully responsive Dashboard with stunning interactive Charts",
     tags: ["Next.js", "Typescript", "Tailwindcss", "shadcn"],
